d67d6fda42de672db249b5ec6b268269415ae8a2
Maysam Hafezparast <maysam@kidocode.com>
2020-11-02 01:17:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Q_type():
     shortreply = "shortreply"
 
 def A_math_question():
-        return {"photo": "44", "topic": 55, "reason":"have fun3"}
+        return {"photo": "44", "topic": 55, "reason":"have fun4"}
 
 names = {"tim": {"age": 22, "gender": "male"},
          "bill": {"age": 20, "gender": "male"},
