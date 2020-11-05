d29f3f23b8164e7cad21a87066225a6c7fed0cb0
yvonneyee <61497190+yvonneyee@users.noreply.github.com>
2020-11-05 10:13:28 +0800

    Update Grade 8 Geometry.py

--- questions/Grade 8 Geometry.py
+++ questions/Grade 8 Geometry.py
@@ -134,3 +134,78 @@ for i in range(23,26):
   print(i,".")
   question6()
   print("")
+
+a=randint(100,190)
+interiorAngles=[144,150,175,162,174]
+#interiorAngles=[60,90,108,120,135,140,144,150,175,162,174]
+def questions7():
+  for i in range(len(interiorAngles)):
+
+    print(i+1,'- Calculate the size of the exterior angles of a regular polygon which has interior angles of',interiorAngles[i])
+
+    print('correct answer =',180-interiorAngles[i])
+    
+    print('')
+    print('wrong answer =',(180-interiorAngles[i])-1)
+    print('wrong answer =',(180-interiorAngles[i])+1)
+    print('wrong answer =',a)
+    print('')
+
+questions7()
+
+a=randint(100,190)
+#regularPolygon=['hexagon','pentagon','octagon','nonagon','decagon']
+sidesPolygon=[6,5,8,9,10]
+def questions8():
+  for i in range(len(sidesPolygon)):
+
+    print(5+(i+1),'- Calculate the size of the exterior and interior angles of ',regularPolygon[i])
+
+    print('correct exterior =',360/sidesPolygon[i],'| correct interior =',180-(360/sidesPolygon[i]))
+    print('')
+    print('wrong answer =',(180-sidesPolygon[i])-1)
+    print('wrong answer =',(180-sidesPolygon[i])+1)
+    print('wrong answer =',a)
+    print('')
+
+questions8()
+
+a=randint(100,190)
+interiorAngles=[156,165,172,175,176,150,175,162,171,174]
+exteriorAngles=[24,15,8,5,4,30,5,18,9,6]
+def questions9():
+  for i in range(len(interiorAngles)):
+
+    sides=360/exteriorAngles[i]
+
+
+    print(10+(i+1),'- Calculate the number of sides of a regular polygon with interior angles of ',interiorAngles[i])
+
+    print('correct side =',sides)
+    print('')
+    print('wrong answer =',(180-exteriorAngles[i])-1)
+    print('wrong answer =',(180-exteriorAngles[i])+1)
+    print('wrong answer =',a)
+    print('')
+
+questions9()
+
+a=randint(100,190)
+interiorAngles=[156,165,172,175,176,150,175,162,171,174]
+exteriorAngles=[24,15,8,5,4,30,5,18,9,6]
+def questions10():
+  for i in range(len(interiorAngles)):
+
+    sides=360/exteriorAngles[i]
+
+
+    print(10+(i+1),'- Calculate the number of sides of a regular polygon with interior angles of ',interiorAngles[i])
+
+    print('correct side =',sides)
+    print('')
+    print('wrong answer =',(180-exteriorAngles[i])-1)
+    print('wrong answer =',(180-exteriorAngles[i])+1)
+    print('wrong answer =',a)
+    print('')
+
+questions10()
